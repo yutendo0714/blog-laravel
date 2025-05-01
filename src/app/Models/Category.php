c0638f2c0c5f098d190488459ce29b942b63a236
yutendo <mayuyuto0714@gmail.com>
2025-05-02 02:14:57 +0900

    リレーションを加味したページ表示にする

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -13,4 +13,15 @@ class Category extends Model
     {
         return $this->hasMany(Post::class);
     }
+
+    public function getPaginateByLimit(int $id, int $limit = 10)
+    {
+        // dd(gettype($this));
+        // $categories = new Category();
+        $category = $this->find($id);
+        // dd(gettype(Category::all()));
+        $posts = $category->posts()->orderBy('updated_at', 'DESC')->paginate($limit);
+        // dd($posts);
+        return $posts;
+    }
 }
